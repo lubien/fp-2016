0f2a03e8e97956c8d27ab83c1252ab3b091fe4ae
lubien <lubien1996@gmail.com>
2016-09-01 15:59:53 -0300

    SAVEPOINT

--- interest.hs
+++ interest.hs
@@ -1,5 +1,5 @@
-simple :: Float -> Float -> Int -> Float
-simple amount _ 0 =
+compound :: Float -> Float -> Int -> Float
+compound amount _ 0 =
   amount
-simple amount tax times =
-  simple (amount + (amount * tax / 100)) tax (times - 1)
+compound amount tax times =
+  compound (amount + (amount * tax / 100)) tax (times - 1)
